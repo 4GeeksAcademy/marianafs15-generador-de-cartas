950e9f7d173393f9f127818b898e45226fe43a1e
Mariana Hueck <marianahueck@gmail.com>
2024-07-30 20:05:01 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector(".card");
+  let num = document.querySelector(".number");
+  let suits = ["hearts", "diamonds", "spades", "clubs"];
+  let ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  function getRandInt(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+  card.classList.add(suits[getRandInt(suits)]);
+  num.innerHTML = ranks[getRandInt(ranks)];
 };
